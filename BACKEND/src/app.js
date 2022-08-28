2c5edf70028bfe3c830f19676bb34d204fa7b153
Dineth Dissanayake <77220940+Dineth-Dissanayake@users.noreply.github.com>
2022-08-28 15:55:46 +0530

    INTIAL

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import {connect} from './utils/db.connection';
+
+const app = express();
+const PORT = process.env.PORT || "8080";
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+    res.send("<h2>📚 Library Management System</h2>");
+    next();
+});
+
+app.listen(PORT, () => {
+    console.log('🚀 SERVER IS UP & RUNNING ON:', PORT);
+    connect();
+});
