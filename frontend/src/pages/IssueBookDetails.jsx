c6f09f8c4b62b6dded9b87c98d5c7535d5239d4f
Chandima Rathnayake <ezonline00@gmail.com>
2022-10-21 16:04:40 +0530

    Final

--- frontend/src/pages/IssueBookDetails.jsx
+++ frontend/src/pages/IssueBookDetails.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import generatePDFIssueBooks from '../components/pdf/repotejenarater';
+import generatePDFIssueBooks from '../components/pdf/repotejenaraterIssueBook';
 
 import Sidebar from '../components/sidebar/Sidebar';
 import "./Main.css";
